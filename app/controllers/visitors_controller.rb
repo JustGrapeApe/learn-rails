8e8c52518f087aaaf51a058d3164e6b9e6ca9b08
JustGrapeApe <al@afedor.com>
2015-11-12 07:33:47 -0500

    Update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,11 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		render 'vistors/new'  #this render is normally 'hidden', i put this in to remind me
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My Birthday is soon.'
+		#flash.now[:arbitrary] = 'How about some presents?'
+		flash.now[:warning] = 'Do not wear black when walking on the road at night!'
+		# rails doesn use warning, stick with notice and alert
+		render 'visitors/new'  #this render is normally 'hidden', i put this in to remind me
 	end
 end
